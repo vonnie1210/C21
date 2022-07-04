233692cdb597a8f99b3824cf91b8d2308418dacd
vonnie1210 <105844890+vonnie1210@users.noreply.github.com>
2022-07-04 17:19:50 +1000

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,21 @@
+class Ground{
+    constructor(x,y,w,h){
+        var options = {
+            isStatic: true
+        }
+        this.body = Bodies.rectangle(x,y,w,h,options);
+        this.w = w;
+        this.h = h;
+        World.add(world, this.body);
+    }
+
+    show(){
+        var pos = this.body.position;
+        push();
+        rectMode(CENTER);
+        stroke(255);
+        fill(130);
+        rect(pos.x, pos.y, this.w, this.h);
+        pop();
+    }
+}
